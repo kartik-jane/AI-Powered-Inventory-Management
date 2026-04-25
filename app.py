09b652336c70f48d25204029fd71bf4cdb8f1a94
SUJAYSAKHARE22 <sujaysakhare22@gmail.com>
2026-04-25 12:56:09 +0530

    set host name for deployment

--- app.py
+++ app.py
@@ -1678,4 +1678,4 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()
         seed_sample_data()
-    app.run(debug=True, port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=True)
